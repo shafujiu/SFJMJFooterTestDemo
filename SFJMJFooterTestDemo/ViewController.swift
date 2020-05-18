139bb16025b82dc7235e331d8a9d1c504c86b1c7
shafujiu <zhaowei200711@163.com>
2020-05-18 16:07:08 +0800

    first commit

---
+++ SFJMJFooterTestDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFJMJFooterTestDemo
+//
+//  Created by shafujiu on 2020/5/18.
+//  Copyright © 2020 shafujiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
